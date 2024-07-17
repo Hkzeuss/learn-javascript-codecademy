2abb8bcd8c04193a394892b9f7444fbd94b5bb8f
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-07-17 20:47:32 +0700

    Complete 2.1.2.Console.js

---
+++ 2. Introduction/2.1. Introductions To JavaScript/2.1.2.Console.js
@@ -0,0 +1,19 @@
+/* Bảng điều khiển (The console) là một bảng điều khiển hiển thị các thông báo quan trọng, như lỗi, cho các nhà phát triển.
+Rất nhiều công việc mà máy tính thực hiện với mã của chúng ta là vô hình với chúng ta mặc định.
+Nếu chúng ta muốn thấy các điều xuất hiện trên màn hình của chúng ta, chúng ta có thể in, hoặc ghi ra log, trực tiếp vào bảng điều khiển của chúng ta 
+*/
+
+/* Trong JavaScript, từ khóa console đề cập đến một đối tượng, một tập hợp các dữ liệu và hành động, mà chúng ta có thể sử dụng trong mã của chúng ta.
+Từ khóa là những từ được tích hợp sẵn trong ngôn ngữ JavaScript, vì vậy máy tính nhận diện chúng và xử lý 1 cách đặc biệt.
+*/
+
+/* Một hành động hay phương thức, được tích hợp trong đối tượng console là phương thức ".log()". Khi chúng ta viết "console.log()", những gì chúng ta đặt trong dấu ngoặc đơn sẽ được in, hoặc ghi log vào bảng điều khiển.
+*/
+
+/* Nó sẽ hữu ích cho chúng ta khi in giá trị ra bảng điều khiển, để chúng ta có thể thấy công việc mà chúng ta làm.
+*/
+
+console.log("Hello"); //in ra Hello
+console.log(2003); //in ra số 2003
+
+// Trong JavaScript thường k bắt buộc phải có dấu ";" ở cuối. Nhưng tôi khuyên bạn nên thêm để không bỏ sót nếu có trường hợp cần thiết.
