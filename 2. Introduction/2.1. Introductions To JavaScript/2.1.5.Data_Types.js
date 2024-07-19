aa057cd6d6cad5dbc332992e72cb1bdd740b1ac4
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-07-19 15:55:37 +0700

    Complete 2.1.5.Data_Types.js

---
+++ 2. Introduction/2.1. Introductions To JavaScript/2.1.5.Data_Types.js
@@ -0,0 +1,38 @@
+// Các dữ liệu là các phân loại mà chúng ta gán cho các loại dữ liệu khác nhau mà chúng ta sử dụng trong lập trình.
+// Trong JavaScript, có tám loại dữ liệu cơ bản:
+
+// 1. Number: Bất kỳ số nào, bao gồm cả số có phần thập phân: 4, 8, 1516, 23.45
+
+// 2. Biglnt: Bất kỳ số nào lớn hơn 2^53 - 1 hoặc nhỏ hơn -(2^53 - 1), với chữ "n" gắn vào số: 1234567890123456n
+
+/* 3. String: Bất kỳ nhóm ký tự nào trên bàn phím của bạn (chữ cái, chữ số, khoảng trắng, ký hiệu, .v.v).
+Được bao quanh bởi dấu nháy đơn '....' hoặc dấu nháy kép "...", mặc dù chúng tôi thích dấu nháy đơn hơn.
+Một số người thích coi chuỗi như 1 từ fancy cho văn bản.
+*/  
+
+/* 4. Boolean: Kiểu dữ liệu này chỉ có hai giá trị có thể là: True - False (Không có dấu nháy).
+Thật hữu ích khi nghĩ về boolean như là công tắc bật/tắt hoặc như là câu trả lời cho 1 câu hỏi "có" hoặc "không".
+*/
+
+/* 5. Null: Kiểu dữ liệu này đại diện cho sự vắng mặt có chủ đích của một giá trị và được đại diện bởi từ khóa null (không có dấu nháy).
+*/
+
+/* 6. Undefined: Kiểu dữ liệu này đại diện cho sự vắng mặt của một biến chưa được gán giá trị (giá trị được đưa ra không tồn tại) và được đại diện bởi từ khóa undefined (không có dấu nháy).
+*/
+
+/* 7. Symbol: Một tính năng mới hơn của ngôn ngữ, các symbol là các định danh duy nhất, hữu ích trong lập trình phức tạp hơn. Hiện tại, không cần phải lo lắng về chúng.
+*/
+
+// 8. Object: Các tập hợp dữ liệu quan trọng.
+
+/* Trong 8 kiểu dữ liệu trên, thì 7 kiểu dữ liệu đầu tiên được coi là các kiểu dữ liệu Nguyên thủy. Chúng là các kiểu dữ liệu cơ bản nhất trong ngôn ngữ
+Các đối tượng thì phức tạp hơn, và bạn sẽ được học thêm nhiều về chúng khi bạn tiếp tục học JS.
+Ban đầu, 8 kiểu có thể không có vẻ nhiều, nhưng sớm thôi bạn sẽ nhận thấy thế giới mở ra với các khả năng khi bạn bắt đầu khai thác từng loại.
+Khi bạn học thêm về đối tượng, bạn sẽ có khả năng tạo ra các tập hợp dữ liệu phức tạp.
+*/
+
+// Nhưng trước đó thì hãy làm quen với số và chuỗi trước nhé.
+
+console.log("Hello World");
+console.log(2011);
+console.log(20.50);
