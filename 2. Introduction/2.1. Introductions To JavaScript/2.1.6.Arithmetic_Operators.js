6095852a4402b9128d287bb40198f6a76e3f96aa
Hkzeuss <Hoangtientrungkien2k3@gmail.com>
2024-07-19 16:16:06 +0700

    Complete 2.1.6.Arithmetic_Operators.js

---
+++ 2. Introduction/2.1. Introductions To JavaScript/2.1.6.Arithmetic_Operators.js
@@ -0,0 +1,39 @@
+// CÁC TOÁN TỬ SỐ HỌC
+
+// Các phép toán cơ bản thường rất hữu ích khi lập trình
+
+/* Một toán tử là một ký tự thực hiện một nhiệm vụ trong mã của chúng ta.
+JavaScript có nhiều toán tử số học tích hợp sẵn, cho phép chúng ta thực hiện các phép toán học trên các số.
+Các toán tử này bao gồm các toán tử và ký hiệu tương ứng sau:
+    +: Phép cộng
+    -: Phép trừ
+/   *: Phép nhân
+    /: Phép chia
+    %: Phép chia lấy phần dư
+    **: Phép mũ (phép cộng các số theo cấp độ)
+    ++: Phép tăng giá trị của biến lên một
+    --: Phép giảm giá trị của biến xuống một
+    +=: Phép cộng và gán giá trị
+    -=: Phép trừ và gán giá trị
+/   *=: Phép nhân và gán giá trị
+    /=: Phép chia và gán giá trị
+    %=: Phép chia lấy phần dư và gán giá trị
+    **=: Phép mũ và gán giá trị
+    ():
+*/
+
+// Bốn toán tử cơ bản nhất có thể hoạt động như sau:
+console.log(1 + 2); // In ra 3
+console.log(2 - 1); // In ra 1
+console.log(3 * 2); // In ra 6
+console.log(9 / 3); // In ra 3
+
+
+/* Lưu ý rằng khi chúng ta sử dụng "console.log()", máy tính sẽ đánh giá biểu thức trong dấu ngoặc đơn (...) và in ra kết quả ở bảng điều khiển.
+Nếu chúng ta muốn in các ký tự "3 + 4", chúng ta sẽ phải học chúng trong dấu nháy "..." và in chúng dưới dạng chuỗi.
+*/
+
+console.log(11 % 3); // In ra 2
+console.log(12 % 3); // In ra 0
+/* Toán tử dư, đôi khi được gọi là modulo, trả về số dư của kết quả chia đó. 11 / 3 thì được 3 và dư 2. Nên kết quả cuối cùng sẽ là 2 và in ra màn hình.
+*/
